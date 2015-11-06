a31215bb48eec991ef812b90ca8c7b1da1b8b665
theCalcaholic <3knoeppl@informatik.uni-hamburg.de>
2015-11-06 17:57:02 +0100

    Fixed method get_delta of classes HiddenLayer and OutputLayer

--- Woche2/mlp/OutputLayer.py
+++ Woche2/mlp/OutputLayer.py
@@ -3,4 +3,9 @@ from . import PerceptronLayer
 
 class OutputLayer(PerceptronLayer):
     def __init__(self, in_size, out_size, activation_fn, activation_fn_deriv):
-        super(in_size, out_size, activation_fn, activation_fn_deriv)
+        super(in_size, out_size,
+              activation_fn=PerceptronLayer.activation_sigmoid,
+              activation_fn_deriv=PerceptronLayer.activation_sigmoid_deriv)
+
+    def get_delta(self, result, error):
+        return error * self.activation_deriv(result)

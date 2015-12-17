f40a02b8e3a9a19d5bb30f8ccc3eda44b7c58b9d
theCalcaholic <tobias@knoeppler.net>
2015-12-18 00:44:07 +0100

    Added basic structure of LSTM training procedure.

--- lstm_network/LSTMNetwork.py
+++ lstm_network/LSTMNetwork.py
@@ -1,46 +1,96 @@
 import numpy as np
 from LSTMLayer import LSTMLayer
-from neural_network.NeuralLayer import NeuralLayer as Layer
+from ..neural_network.NeuralLayer import NeuralLayer as Layer
 
 
 class LSTMNetwork:
+
     def __init__(self):
         self.layers_spec = None
         self.layers = None
         self.populated = False
         self.trained = False
+        self.chars = None
+        self.char_to_int = None
+        self.int_to_char = None
+        self.state_history = None
 
-    def populate(self, in_size, layer_sizes=None):
+    def populate(self, char_set, layer_sizes=None):
         layer_sizes = layer_sizes or self.layers_spec or []
+        chars = list(char_set)
+        if len(chars) == 0:
+            raise Exception("first argument char_set is empty!")
+        self.chars = chars
+        self.char_to_int = {ch: i for i, ch in enumerate(self.chars)}
+        self.int_to_char = {i: ch for i, ch in enumerate(self.chars)}
+
         self.layers = []
         self.layers.append(LSTMLayer(
-            in_size=in_size,
+            in_size=len(self.chars),
             out_size=layer_sizes[0]))
 
         if len(layer_sizes) != 0:
-            for cur_size in layer_sizes[1:-1]:
+            for cur_size in layer_sizes[1:]:
                 self.layers.append(LSTMLayer(
                     in_size=self.layers[-1].size,
                     out_size=cur_size))
         self.layers.append(Layer(
             in_size=self.layers[-1].size,
-            out_size=layer_sizes[-1],
+            out_size=len(self.chars),
             activation_fn=Layer.activation_linear,
             activation_fn_deriv=Layer.activation_linear_deriv))
         self.populated = True
 
-    def feedforward(self, data):
+    def feedforward(self, inputs):
         if not self.populated:
             raise Exception("LSTM network must be populated first (Have a look at method LSTMNetwork.populate)!")
 
-        results = [data]
-        for layer in self.layers[:-1]:
-            results.append(
-                layer.feed(results[-1])
-            )
-        results.append(
-            self.layers[-1].feed(results[-1].T)
-        )
+        states, results, enc_inputs, enc_outputs = {}, {}, {}, {}
+        states[-1] = [layer.state for layer in self.layers[:-1]]  # get initial state of all lstm layers
+        for i in xrange(len(inputs)):
+            enc_inputs[i] = np.zeros((len(self.chars), 1))  # encode character in 1-of-k representation
+            enc_inputs[i][i] = 1
+            results[i][-1] = [inputs[i]]
 
-        return results
+            # feed forward through all layers
+            for j in xrange(len(self.layers) - 1):
+                results[i][j] = self.layers[j].feed(results[i][j-1]) # get output of each lstm layer
+            results[i][len(self.layers)] = self.layers[-1].feed(results[len(self.layers) - 1].T)  # add output of lstm network to results
+            states[i] = [layer.state for layer in self.layers[:-1]] # get updated state from all lstm layers
+
+        return results, states
+
+    def backpropagate(self, inputs, targets, results, states):
+        deltas = [np.zeros_like(layer.weights) for layer in self.layers]
+        deltas.append(0)
+        delta_state_next = np.zeros_like(states[0])
+        for i in reversed(xrange(len(inputs))):
+            deltas[-1] = np.copy(results[i])
+            deltas[-1][targets[i]] -= 1
+            for j in reversed(xrange(len(deltas) - 1)):
+                deltas[j] += np.dot(deltas[j+1], states[i].T)
+
+            for result, layer, last_weights in reversed(xrange(len(self.layers)-1)):
+                deltas.append(
+                    self.layers[-j-1].get_delta(
+                        result[i][-j-1],
+                        deltas[-j-1],
+                        self.layers.weights[-j-1]
+                    )
+                )
+
+
+    def train(self, t_input, seq_length):
+        p = 0
+        smooth_loss = -np.log(1.0/len(self.chars)) * seq_length
+        while True:
+            inputs = [self.char_to_int[ch] for ch in t_input[p:p+seq_length]]
+            targets = [self.char_to_int[ch] for ch in t_input[1+p:1+p+seq_length]]
+            enc_inputs, enc_outputs = {}
+
+            for i in xrange(len(inputs)):
+                enc_inputs[i] = np.zeros((len(self.chars), 1))
+                enc_inputs[i][inputs[i]] = 1
+
+
 
--- multi_layer_perceptron/MultiLayerPerceptron.py
+++ multi_layer_perceptron/MultiLayerPerceptron.py
@@ -1,6 +1,6 @@
 import numpy
 
-from neural_network.NeuralLayer import NeuralLayer as Layer, BiasedNeuralLayer as BiasedLayer
+from ..neural_network.NeuralLayer import NeuralLayer as Layer, BiasedNeuralLayer as BiasedLayer
 
 # directory for observing the results
 path_obs = "res/"
@@ -8,14 +8,12 @@ path_obs = "res/"
 
 class MultiLayerPerceptron:
 
-    """Creates a new instance of class MLP
-    @size_in: number of input neurons
-    @size_out: number of output neurons
-    @size_hid: number of hidden layer neurons"""
-    def __init__(self,
-                 in_size=0,
-                 layers=None,
-                 config=dict()):
+    def __init__(self, in_size=0, layers=None, config=dict()):
+        """Creates a new instance of class MLP
+        @size_in: number of input neurons
+        @size_out: number of output neurons
+        @size_hid: number of hidden layer neurons"""
+
         assert isinstance(config, dict)
         self.populated = False
         self.trained = False
@@ -57,8 +55,9 @@ class MultiLayerPerceptron:
             activation_fn_deriv=Layer.activation_linear_deriv))
         self.populated = True
 
-    """applies the MLP network to a set of input data and returns a list of the outputs of each perceptron layer"""
     def feedforward(self, data):
+        """applies the MLP network to a set of input data and returns a list of the outputs of each perceptron layer"""
+
         if not self.populated:
             raise Exception("MLP must be populated first (Have a look at method MLP.populate())!")
 
@@ -70,13 +69,14 @@ class MultiLayerPerceptron:
 
         return results
 
-    """calculates and applies the change in weight according to the error resulting from feedforward and the
-    learning rate provided
-    returns the absolute error
-    @results: An array of layer wise results (as produced by feedforward)
-    @target_output: A vector of output values to be used as training reference
-    @learning_rate: Influences the performance (the higher the better) and accuracy (the lower the better)"""
     def backpropagate(self, results, target_output):
+        """calculates and applies the change in weight according to the error resulting from feedforward and the
+        learning rate provided
+        returns the absolute error
+        @results: An array of layer wise results (as produced by feedforward)
+        @target_output: A vector of output values to be used as training reference
+        @learning_rate: Influences the performance (the higher the better) and accuracy (the lower the better)"""
+
         if not self.populated:
             raise Exception("MLP must be populated first (Have a look at method MLP.populate())!")
 
@@ -105,12 +105,13 @@ class MultiLayerPerceptron:
         # returns the absolute error (distance of target output and actual output)
         return error
 
-    """Trains the MLP with the given training data
-    @t_input: Set of training input vectors
-    @t_output: Set of training output vectors matching t_input
-    @iterations: number of training epochs
-    @learning_rate: Influences the performance (the higher the better) and accuracy (the lower the better)"""
     def train(self, t_input, t_output):
+        """Trains the MLP with the given training data
+        @t_input: Set of training input vectors
+        @t_output: Set of training output vectors matching t_input
+        @iterations: number of training epochs
+        @learning_rate: Influences the performance (the higher the better) and accuracy (the lower the better)"""
+
         t_input = t_input
         for i in range(self.config["max_iterations"]):
             errors = []
@@ -127,8 +128,9 @@ class MultiLayerPerceptron:
                 return
         self.trained = True
 
-    """Returns predicted output vector for a given input vector data_in"""
     def predict(self, data_in):
+        """Returns predicted output vector for a given input vector data_in"""
+
         if not self.trained:
             raise Exception("MLP must be trained first!")
         return self.feedforward(data_in)[-1][0]
@@ -139,30 +141,33 @@ class MultiLayerPerceptron:
             print("  shape: " + str(numpy.shape(self.layers[i].weights)))
             print("  activation_fn: " + str(self.layers[i].activation))
 
-    """Adds a hidden layer to the MLP.
-    @size: the number of nodes the new layer will have
-    @fn: the layers activation function
-    @fn_deriv: the layers activation functions derivation"""
     def add_layer(self, size, fn, fn_deriv):
+        """Adds a hidden layer to the MLP.
+        @size: the number of nodes the new layer will have
+        @fn: the layers activation function
+        @fn_deriv: the layers activation functions derivation"""
+
         self.layers_specs.append({
             "size": size,
             "fn": fn,
             "fn_deriv": fn_deriv
         })
 
-    """Adds a list of hidden layers to the MLP. Each layer must be a dict containing
-        the keys 'size', 'fn' and 'fn_deriv'"""
     def add_layers(self, layer_list):
+        """Adds a list of hidden layers to the MLP. Each layer must be a dict containing
+            the keys 'size', 'fn' and 'fn_deriv'"""
+
         for layer in layer_list:
             self.add_layer(layer["size"], layer["fn"], layer["fn_deriv"])
 
-    """Expects a dictionary of configuration options to update the existing values. Possible options are:
-    learning_rate (float): Determines how fast and precise the network is going to learn -
-            higher learning rate means faster learning and lower learning precision.
-    target_precision (float): Sets the target precision for early stopping. To disable early stopping at all,
-            set this value to 0.
-    max_iterations (float): The maximum number of iterations over which the MLP should be trained"""
     def configure(self, config):
+        """Expects a dictionary of configuration options to update the existing values. Possible options are:
+        learning_rate (float): Determines how fast and precise the network is going to learn -
+                higher learning rate means faster learning and lower learning precision.
+        target_precision (float): Sets the target precision for early stopping. To disable early stopping at all,
+                set this value to 0.
+        max_iterations (float): The maximum number of iterations over which the MLP should be trained"""
+
         assert isinstance(config, dict)
         assert "learning_rate" not in config or isinstance(config["learning_rate"], float) \
             and config["max_iterations"] > 0

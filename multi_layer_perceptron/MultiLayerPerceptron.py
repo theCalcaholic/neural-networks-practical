2e0a6a5921cb6c752c3bab7bf29e0398727c40c7
theCalcaholic <3knoeppl@informatik.uni-hamburg.de>
2016-01-15 13:39:58 +0100

    Revert "Revert "Added branch for Elman network and class for Elman Hidden Layer""
    This reverts commit c2e02acb6fc14016999dde57817bf3306a7b6f59.

--- multi_layer_perceptron/MultiLayerPerceptron.py
+++ multi_layer_perceptron/MultiLayerPerceptron.py
@@ -1,6 +1,7 @@
 import numpy
 
 from neural_network.NeuralLayer import NeuralLayer as Layer, BiasedNeuralLayer as BiasedLayer
+from neural_network.ElmanHiddenLayer import ElmanHiddenLayer
 
 # directory for observing the results
 path_obs = "res/"
@@ -39,37 +40,35 @@ class MultiLayerPerceptron:
         self.layers.append(Layer(
             in_size=in_size,
             out_size=layers[0]["size"],
+            predecessor=None,
+            successor=None,
             activation_fn=layers[0]["fn"] or Layer.activation_linear,
             activation_fn_deriv=layers[0]["fn_deriv"] or Layer.activation_linear_deriv))
 
         if len(layers) != 0:
             for cur_layer in layers[1:-1]:
-                self.layers.append(BiasedLayer(
+                self.layers[-1].add_successor(ElmanHiddenLayer(
                     in_size=self.layers[-1].size,
                     out_size=cur_layer["size"],
+                    predecessors=[self.layers[-1]],
                     activation_fn=cur_layer["fn"] or Layer.activation_sigmoid,
                     activation_fn_deriv=cur_layer["fn_deriv"] or Layer.activation_sigmoid_deriv))
+                self.layers.append(self.layers[-1].successor)
 
-        self.layers.append(Layer(
+        self.layers[-1].add_successor(Layer(
             in_size=self.layers[-1].size,
             out_size=layers[-1]["size"],
             activation_fn=Layer.activation_linear,
-            activation_fn_deriv=Layer.activation_linear_deriv
-        ))
+            activation_fn_deriv=Layer.activation_linear_deriv))
+        self.layers.append(self.layers[-1].successor)
         self.populated = True
 
-    """applies the MLP network to a set of input data and returns a list of the outputs of each perceptron layer"""
     def feedforward(self, data):
+        """applies the MLP network to a set of input data and returns a list of the outputs of each perceptron layer"""
         if not self.populated:
             raise Exception("MLP must be populated first (Have a look at method MLP.populate())!")
 
-        results = [data]
-        for layer in self.layers:
-            results.append(
-                layer.feed(results[-1])
-            )
-
-        return results
+        return self.layers[0].feed(data)
 
     """calculates and applies the change in weight according to the error resulting from feedforward and the
     learning rate provided
@@ -77,7 +76,7 @@ class MultiLayerPerceptron:
     @results: An array of layer wise results (as produced by feedforward)
     @target_output: A vector of output values to be used as training reference
     @learning_rate: Influences the performance (the higher the better) and accuracy (the lower the better)"""
-    def backpropagate(self, results, target_output):
+    def backpropagate(self, target_outputs):
         if not self.populated:
             raise Exception("MLP must be populated first (Have a look at method MLP.populate())!")
 
--- neural_network/ElmanHiddenLayer.py
+++ neural_network/ElmanHiddenLayer.py
@@ -1,26 +1,75 @@
 from NeuralLayer import NeuralLayer, BiasedNeuralLayer
+from LayerCache import LayerCache
 import numpy as np
 
 
 class ElmanHiddenLayer(BiasedNeuralLayer):
 
-    def __init__(self, in_size, out_size, activation_fn, activation_fn_deriv):
+    def __init__(self,
+                 in_size, out_size,
+                 predecessors={}, successors={}, activation_fn=None, activation_fn_deriv=None):
+        self.predecessors = {'input': None, 'context': None}
+        self.successors = {'output': None, 'context': None}
+        self.predecessors = predecessors
+        self.successors = successors
+        self.cache
+        self.in_size = in_size
+        self.size = out_size
         self.context_layer = BiasedNeuralLayer(
-                in_size=out_size,
+                in_size=in_size,
                 out_size=out_size,
                 activation_fn=NeuralLayer.activation_sigmoid,
                 activation_fn_deriv=NeuralLayer.activation_sigmoid_deriv)
-        self.context = np.zeros((out_size, 1))
-        super(self.__class__).__init__(in_size + out_size, out_size, activation_fn, activation_fn_deriv)
+        self.predecessors['context'] = self.context_layer
+        self.successors['context'] = self.context_layer
+        if len(predecessors) >= 2 and isinstance(predecessors[1], ElmanHiddenLayer):
+            self.weights = predecessors['hidden'].weights
+            self.biases = predecessors['hidden'].biases
+            self.context_layer.weights = predecessors['hidden'].context_layer.weights
+            self.context_layer.biases = predecessors['hidden'].context_layer.biases
+        else:
+            super(self.__class__).__init__(
+                    in_size + out_size,
+                    out_size,
+                    predecessors,
+                    successors,
+                    activation_fn or NeuralLayer.activation_sigmoid,
+                    activation_fn_deriv or NeuralLayer.activation_sigmoid_deriv)
 
     def feed(self, data):
-        output = super(self.__class__).feed(
+        self.cache.output = super(self.__class__).feed(
                 np.vstack((
                     data,
-                    self.context)))
-        self.context = self.context_layer.feed(output)
-        return output
+                    self.predecessors['context'].cache.output)))
+        self.successors['context'].feed(self.cache.output)
+        return self.successors['output'].feed(self.cache.output)
 
     def learn(self, result, delta, learning_rate):
         # TODO: Backpropagation over time
         super(self.__class__).learn(result="", delta="", learning_rate="")
+
+    def unfold(self, depth):
+        if depth == 0:
+            return
+
+        successor = ElmanHiddenLayer(
+                self.in_size,
+                self.size,
+                predecessors={'input': self.predecessors['input'], 'hidden': self},
+                successors={'output': self.successors['output']},
+                activation_fn=self.activation,
+                activation_fn_deriv=self.activation_deriv)
+        self.successors['hidden'] = successor
+        self.successors['hidden'].unfold(depth - 1)
+
+    def add_successor(self, successor):
+        if isinstance(successor, ElmanHiddenLayer):
+            self.successors['hidden'] = successor
+        else:
+            self.successors['output'] = successor
+
+    def add_predecessor(self, predecessor):
+        if isinstance(predecessor, ElmanHiddenLayer):
+            self.predecessors['hidden'] = predecessor
+        else:
+            self.predecessors['input'] = predecessor

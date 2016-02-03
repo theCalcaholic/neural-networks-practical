Fixed backpropagation for multiple LSTM Layers

lstm_example_wordset.py
@@ -18,9 +18,9 @@ freestyle: juauee emusuari d iasooeutd cor
 """
 
 config = {
-    "memory_size": 200,
-    "time_steps": 10,
-    "learning_rate": 0.01,
+    "memory_sizes": [150, 100],
+    "time_steps": 20,
+    "learning_rate": 0.1,
     "iterations": 10000,
     "target_loss": 0.01,
     "use_output_layer": True,
@@ -47,7 +47,7 @@ data_store.set_input_text(input_text)
 
 
 data_store.configure({
-    "memory_size": config["memory_size"],
+    "memory_size": config["memory_sizes"][0],
     "sequence_length": config["time_steps"],
     "extend_alphabet": False #only necessary if output layer is disabled
 })
@@ -66,7 +66,7 @@ lstm.get_status = util.get_status_function(data_store, lstm, config["status_freq
 lstm.populate(
     in_size=np.shape(samples[0][0])[0],
     out_size=np.shape(samples[0][0])[0],
-    layer_sizes=[data_store.config["memory_size"]])
+    layer_sizes=config["memory_sizes"])
 
 # Uncomment to load previously saved network weights and biases from directory
 #lstm.load(config["save_dir"])

lstm_network/LSTMLayer.py
@@ -93,17 +93,17 @@ class LSTMLayer(object):
         # Uncomment for output layer:
         return cache.output_values
 
-    def learn_recursive(self, cache, deltas, loss_total=0):
+    def learn_recursive(self, cache, deltas):
         if len(deltas) == 0 or cache.is_first_cache:
-            return loss_total
+            return
         # calculate loss and cumulative loss but keep loss for t+1 (last loss)
         delta = deltas[-1]
-        #loss_total += delta ** 2
 
         loss_output = delta + cache.successor.loss_output
 
         last_loss_state = cache.successor.loss_state
 
+        #print(str(np.shape(cache.output_gate_results)) + str(np.shape(loss_output)) + str(np.shape(last_loss_state)))
         delta_state = cache.output_gate_results * loss_output + last_loss_state
 
         delta_output_gate = self.output_gate_layer.activation_deriv(
@@ -144,13 +144,14 @@ class LSTMLayer(object):
         cache.loss_input = delta_concatinated_input[:self.in_size]
         cache.loss_output = delta_concatinated_input[self.in_size:]
 
-        return self.learn_recursive(cache.predecessor, deltas[:-1], loss_total)
+        return self.learn_recursive(cache.predecessor, deltas[:-1])
 
     def learn(self, deltas, learning_rate=0.001):
         Logger.debug("learn(" + str(deltas) + ")")
-        loss = self.learn_recursive(self.last_cache.predecessor, deltas, 0)
+        self.learn_recursive(self.last_cache.predecessor, deltas)
         self.apply_training(learning_rate)
-        return loss
+        deltas = [cache.loss_input for cache in self.caches]
+        return deltas
 
     def apply_training(self, learning_rate):
         p_updates = self.pending_updates
@@ -199,19 +200,11 @@ class LSTMLayer(object):
     def euclidean_loss_function(cls, predicted, target):
         return (predicted - target) ** 2
 
-    def visualize(self, path, prefix=""):
-        self.input_gate_layer.visualize(path, prefix + "InputG")
-        self.forget_gate_layer.visualize(path, prefix + "ForgetG")
-        self.output_gate_layer.visualize(path, prefix + "OutputG")
-        self.update_values_layer.visualize(path, prefix + "UpdateL")
-        """KTimage.exporttiles(self.input_gate_layer.weights, self.in_size + self.size,
-                            1, os.path.join(path, "obs_InputG_1_0.pgm"), 1, self.size)
-        KTimage.exporttiles(self.forget_gate_layer.weights, self.in_size + self.size,
-                            1, os.path.join(path, "obs_ForgetG_2_0.pgm"), 1, self.size)
-        KTimage.exporttiles(self.update_values_layer.weights, self.in_size + self.size,
-                            1, os.path.join(path, "obs_UpdateL_3_0.pgm"), 1, self.size)
-        KTimage.exporttiles(self.output_gate_layer.weights, self.in_size + self.size,
-                            1, os.path.join(path, "obs_OutputG_4_0.pgm"), 1, self.size)"""
+    def visualize(self, path, layer_id):
+        self.input_gate_layer.visualize(os.path.join(path, "LSTM" + str(layer_id), "obs_" + "InputG_" + str(layer_id) + "_0.pgm"))
+        self.forget_gate_layer.visualize(os.path.join(path, "LSTM" + str(layer_id), "obs_" + "ForgetG_" + str(layer_id * 2) + "_0.pgm"))
+        self.output_gate_layer.visualize(os.path.join(path, "LSTM" + str(layer_id), "obs_" + "OutputG_" + str(layer_id * 3) + "_0.pgm"))
+        self.update_values_layer.visualize(os.path.join(path, "LSTM" + str(layer_id), "obs_" + "UpdateL_" + str(layer_id * 4) + "_0.pgm"))
 
     def clear_cache(self):
         self.caches = []

lstm_network/LSTMNetwork.py
@@ -2,7 +2,6 @@ import os
 import numpy as np
 from lstm_network.LSTMLayer import LSTMLayer
 from neural_network import NeuralLayer, CachingNeuralLayer
-import KTimage
 
 
 class LSTMNetwork:
@@ -33,7 +32,7 @@ class LSTMNetwork:
         ))
         for layer_size in layer_sizes[1:]:
             self.training_layers.append(LSTMLayer(
-                in_size=in_size,
+                in_size=self.training_layers[-1].size,
                 memory_size=layer_size
             ))
         self.output_layer = CachingNeuralLayer(
@@ -92,10 +91,13 @@ class LSTMNetwork:
         deltas = [
             np.dot(self.output_layer.weights.T, output_loss)
             for output_loss in output_losses]
+
+        #print("deltas 3: " + str(np.shape(deltas)))
         self.output_layer.weights -= pending_output_weight_updates * self.config["learning_rate"]
 
-        for layer in self.training_layers[:-1]:
+        for layer in reversed(self.training_layers[:-1]):
             deltas = layer.learn(deltas, self.config["learning_rate"])
+            #print("deltas x: " + str(np.shape(deltas)))
 
         return error
 
@@ -145,7 +147,7 @@ class LSTMNetwork:
                 loss_diff = iteration_loss
                 if iteration_loss < target_loss:
                     print("Target loss reached! Training finished.")
-                    for layer in self.lstm_training_layers:
+                    for layer in self.training_layers:
                         layer.clear_cache()
                     self.trained = True
                     return
@@ -153,9 +155,9 @@ class LSTMNetwork:
 
     def reroll_weights(self):
         self.populate(
-            in_size=self.lstm_training_layers[0].in_size,
+            in_size=self.training_layers[0].in_size,
             out_size=self.output_layer.size,
-            layer_sizes=[layer.size for layer in self.lstm_training_layers])
+            layer_sizes=[layer.size for layer in self.training_layers])
 
     def predict(self, input_vector):
 
@@ -207,5 +209,6 @@ class LSTMNetwork:
         return (predicted - target) ** 2
 
     def visualize(self, path):
-        for idx, layer in enumerate(self.training_layers):
-            layer.visualize(path, str(idx))
+        for idx, layer in enumerate(self.training_layers[:-1]):
+            layer.visualize(path, idx + 1)
+        self.output_layer.visualize(os.path.join(path, "obs_OutputL_1_0.pgm"))

neural_network/NeuralLayer.py
@@ -2,9 +2,13 @@ import numpy as np
 from lstm_network.util import Logger
 import KTimage
 import os
+from lstm_network import util
 
 class NeuralLayer(object):
     def __init__(self, in_size, out_size, activation_fn, activation_fn_deriv):
+        util.Logger.debug("create Neural Layer: \n" + \
+                          "  in_size: " + str(in_size) + "\n" + \
+                          "  out_size: " + str(out_size))
         self.weights = np.random.uniform(-0.001, 0.001, (out_size, in_size)).astype(np.float64)
         if not hasattr(self, 'biases'):
             self.biases = np.zeros(out_size).astype(np.float64)
@@ -38,9 +42,11 @@ class NeuralLayer(object):
         self.weights = np_saved['weights']
         self.biases = np_saved['biases']
 
-    def visualize(self, path, prefix=""):
+    def visualize(self, path):
+        if not os.path.isdir(os.path.dirname(path)):
+            os.makedirs(os.path.dirname(path))
         KTimage.exporttiles(self.weights, self.in_size,
-                            1, os.path.join(path, "obs_" + prefix + "NLayer_5_0.pgm"), 1, self.size)
+                            1, path, 1, self.size)
 
     @classmethod
     def activation_linear(cls, x):